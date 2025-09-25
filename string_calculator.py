00146388ee0de23853d50177ea7d5ec6839a5c4b
Harshith J.V. <harshithjv@yahoo.co.in>
2025-09-25 22:48:48 +0530

    test: single number in string returns same number

--- string_calculator.py
+++ string_calculator.py
@@ -4,4 +4,10 @@ class StringCalculator:
     def add(self, numbers: str) -> int:
         if numbers == "":
             return 0
+        try:
+            num = int(numbers)
+        except Exception:
+            pass
+        else:
+            return num
         return -1
--- tests.py
+++ tests.py
@@ -5,3 +5,6 @@ str_calc = StringCalculator()
 
 def test_empty_string_return_0():
     assert str_calc.add("") == 0
+
+def test_single_number_returns_number():
+    assert str_calc.add("64") == 64
